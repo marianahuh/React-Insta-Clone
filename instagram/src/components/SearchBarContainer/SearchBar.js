a3073d9702db96cac71eab70ab6df8b1f2845009
Mariana <mhuh0176@gmail.com>
2019-03-29 13:23:51 -0700

    addedd instagram logo

--- instagram/src/components/SearchBarContainer/SearchBar.js
+++ instagram/src/components/SearchBarContainer/SearchBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import IGLogo from '../../assets/instagramBanner.jpg';
+import IGLogo from './img/instagramBanner.jpg';
 import './SearchBar.css';
 
 
@@ -11,20 +11,20 @@ const SearchBar = props => {
         <div className='camera'>
           <i className="fab fa-instagram"></i>
           <div>|</div>
-          <img className='banner' src={IGLogo} />
+          <img className='banner' src={IGLogo} alt='instagram logo' />
         </div>
       </div>
       <div>
         <input type='text' placeholder='Search' />
       </div>
-      <div className='social-wrapper'>
-        <div className='social'>
+      <div className='icon-wrapper'>
+        <div className='icon'>
           <i className='fa fa-compass' />
         </div>
-        <div className='social'>
+        <div className='icon'>
           <i className='fa fa-heart' />
         </div>
-        <div className='social'>
+        <div className='icon'>
           <i className='fa fa-circle' />
         </div>
       </div>
