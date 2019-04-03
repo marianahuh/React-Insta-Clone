2d3820fffac3e9d81bac37d3a8f5a1c95d3f5cb9
Mariana <mhuh0176@gmail.com>
2019-04-02 20:08:07 -0700

    added directory files

---
+++ instagram/src/components/Authentication/Authenticate.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Login from '../Login/LoginPage';
+
+
+const Authenticate = App =>
+  class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+    }
+
+    render() {
+
+    }
+  };
+
+
+export default Authenticate;
---
+++ instagram/src/components/Login/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css';
+
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+};
+
+export default LoginPage;
